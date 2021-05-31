5f53e4401f60dace430fcaa429b5b5179ea5c4f8
Anthony Rivas <arivasc@unsa.edu.pe>
2021-05-31 15:02:46 -0500

    Creado el modelo de la clase Alumno en alumnos/models.py

--- alumnos/models.py
+++ alumnos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    cui = models.TextField()
+    nombres = models.TextField()
+    apellidos = models.TextField()
+    escuela = models.TextField()
