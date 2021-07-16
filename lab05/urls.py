e4d86db7fd41d35ca80b4b117aec3ce074a27473
Anthony Rivas <arivasc@unsa.edu.pe>
2021-07-16 03:06:59 -0500

    Renderizamos una Persona de personas con un nuevo template en templates/personas/test.html.

--- lab05/urls.py
+++ lab05/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from personas.views import personaTestView
 from inicio.views import anotherView, myHomeView
 from django.contrib import admin
 from django.urls import path
@@ -20,5 +21,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myHomeView, name='Pagina de Inicio'),
-    path('another/', anotherView)
+    path('another/', anotherView),
+    path('personas/', personaTestView, name='otro'),
 ]
